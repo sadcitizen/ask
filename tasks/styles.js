0667d8067f93d2a12dcb5fc8f2867396fea856ad
instanceofpro <instanceofpro@gmail.com>
2015-04-18 20:51:37 +0500

    fix for styles basename

--- tasks/styles.js
+++ tasks/styles.js
@@ -5,29 +5,25 @@ var _ = require('lodash'),
     sass = require('gulp-sass'),
     csso = require('gulp-csso'),
     gulpif = require('gulp-if'),
-    gutil = require('gulp-util'),
     rename = require('gulp-rename'),
     header = require('gulp-header'),
     plumber = require('gulp-plumber'),
     prefix = require('gulp-autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
     config = require('../gulp.config'),
-    paths = config.paths;
-
-var defaults = {
-    compress: false,
-    browsers: [],
-    sourcemaps: false,
-    banner: ''
-};
+    paths = config.paths,
+    defaults = {
+        compress: false,
+        browsers: [],
+        sourcemaps: false,
+        banner: ''
+    };
 
 gulp.task('styles', function () {
     _.defaults(config.styles.settings, defaults);
 
-    function build (bundle) {
+    function build(bundle) {
         var settings = _.defaults(bundle.settings || {}, config.styles.settings);
-
-        gutil.log('--> Compiling \'' + gutil.colors.green(bundle.name) + '\' from \'' + gutil.colors.green(bundle.src) + '\'...');
 
         return gulp.src(bundle.src)
             .pipe(plumber())
@@ -35,7 +31,7 @@ gulp.task('styles', function () {
             .pipe(sass())
             .pipe(gulpif(settings.browsers.length, prefix({ browsers: settings.browsers })))
             .pipe(gulpif(settings.compress, csso()))
-            .pipe(gulpif(settings.compress, rename({ suffix: '.min' })))
+            .pipe(gulpif(settings.compress, rename({ basename: bundle.name, suffix: '.min' })))
             .pipe(gulpif(settings.banner.length, header(settings.banner)))
             .pipe(sourcemaps.write('.'))
             .pipe(plumber.stop())

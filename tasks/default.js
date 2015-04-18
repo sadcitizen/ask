6433cf56a5598188283677f23bcfd2b1fe29b3f2
instanceofpro <instanceofpro@gmail.com>
2015-04-18 21:33:03 +0500

    add default task

---
+++ tasks/default.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+gulp.task('default', function () {
+    gutil.log(gutil.colors.red('Здесь будет список доступных команд.'));
+});

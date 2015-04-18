1d83c1a3321817a727a6e952faea14589fe70e41
instanceofpro <instanceofpro@gmail.com>
2015-04-18 20:51:12 +0500

    add reporter for jshint

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -2,13 +2,14 @@
 
 var gulp = require('gulp'),
     hint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
     config = require('../gulp.config');
 
 gulp.task('hint', function () {
     function check(bundle) {
         return gulp.src(bundle.src)
             .pipe(hint('./.jshintrc'))
-            .pipe(hint.reporter('default'));
+            .pipe(hint.reporter(stylish));
     }
 
     config.scripts.bundles.forEach(check);
